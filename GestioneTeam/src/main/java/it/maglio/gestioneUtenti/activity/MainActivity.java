bb7a58735781b02413dc91d57fc130226ec6161e
PC-GMAGLIO\pc ads <giuseppe.maglio@assembly.it>
2017-03-16 12:06:11 +0100

    aggiornato main activity aggiornati nuovi path per connessioni server

--- GestioneTeam/src/main/java/it/maglio/gestioneUtenti/activity/MainActivity.java
+++ GestioneTeam/src/main/java/it/maglio/gestioneUtenti/activity/MainActivity.java
@@ -1,43 +1,44 @@
 package it.maglio.gestioneUtenti.activity;
 
-import android.app.job.JobInfo;
-import android.app.job.JobScheduler;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.os.ParcelFileDescriptor;
 import android.provider.MediaStore;
-import android.provider.Settings;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.widget.Toolbar;
+import android.util.Base64;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileDescriptor;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.math.BigInteger;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import it.giuseppe.app.R;
+import it.maglio.gestioneUtenti.asyncTask.UploadProfileImage;
 import it.maglio.gestioneUtenti.fragment.CambioPasswordFragment;
 import it.maglio.gestioneUtenti.fragment.ListViewFragment;
 import it.maglio.gestioneUtenti.fragment.SignUpFragment;
 import it.maglio.gestioneUtenti.bean.SessionBean;
 import it.maglio.gestioneUtenti.fragment.UserProfileFragment;
-import it.maglio.gestioneUtenti.mqtt.MqttClientUtils;
 import it.maglio.gestioneUtenti.service.MqttListnerService;
-import it.maglio.gestioneUtenti.utility.PathUtils;
 import it.tortuga.beans.User;
-
-import static android.R.attr.bitmap;
 
 
 /**
@@ -64,17 +65,13 @@ public class MainActivity extends GeneralActivity {
     }
 
     private void insertImage() {
-//        Intent intent = new Intent(Intent.ACTION_PICK,
-//                MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-//        intent.setType("image/*");
-//        intent.putExtra("crop", "true");
-//        intent.putExtra("scale", true);
-//        intent.putExtra("outputX", 256);
-//        intent.putExtra("outputY", 256);
-//        intent.putExtra("aspectX", 1);
-//        intent.putExtra("aspectY", 1);
-//        intent.putExtra("return-data", true);
-//        startActivityForResult(intent, 1);
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_PICK);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), 8);
+    }
+
+    private void openMaps() {
         Uri gmmIntentUri = Uri.parse("google.navigation:q=Via Beata Savina Petrilli+Roma,+Roma+Italia");
         Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
         mapIntent.setPackage("com.google.android.apps.maps");
@@ -92,7 +89,7 @@ public class MainActivity extends GeneralActivity {
         setContentView(R.layout.main_activity);
         ButterKnife.bind(this);
         buildMenuDrawerLayout(navigationView.getMenu());
-        startServiceNotifiche();
+      //  startServiceNotifiche();
         TextView username = (TextView) navigationView.getHeaderView(0).findViewById(R.id.username);
         TextView email = (TextView) navigationView.getHeaderView(0).findViewById(R.id.email);
         user = SessionBean.getSessionBean().getUser();
@@ -143,8 +140,10 @@ public class MainActivity extends GeneralActivity {
                         fragmentTransaction2.commit();
                         return false;
                     case R.id.insertImage:
-                        insertImage();
+                        openMaps();
                         return false;
+                    case R.id.insertGalleryImg:
+                        insertImage();
                     default:
                         return true;
                 }
@@ -182,21 +181,35 @@ public class MainActivity extends GeneralActivity {
         if (resultCode != RESULT_OK) {
             return;
         }
-        if (requestCode == 1) {
-            InputStream stream = null;
+        if (requestCode == 8) {
+            Uri uri = data.getData();
+            ParcelFileDescriptor parcelFileDescriptor =
+                    null;
             try {
-                stream = getContentResolver().openInputStream(
-                        data.getData());
-                Bitmap bitmap = BitmapFactory.decodeStream(stream);
-                stream.close();
+                parcelFileDescriptor = getContentResolver().openFileDescriptor(uri, "r");
             } catch (FileNotFoundException e) {
-                Log.e("ERRROR", Log.getStackTraceString(e));
-            } catch (IOException e) {
-                Log.e("ERRROR", Log.getStackTraceString(e));
+                e.printStackTrace();
             }
+            FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+            Bitmap image = BitmapFactory.decodeFileDescriptor(fileDescriptor);
 
+            new UploadProfileImage(getRealPathFromURI(uri)).execute();
         }
 
 
+    }
+
+    public Uri getImageUri(Context inContext, Bitmap inImage) {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
+    }
+
+    public String getRealPathFromURI(Uri uri) {
+        Cursor cursor = getContentResolver().query(uri, null, null, null, null);
+        cursor.moveToFirst();
+        int idx = cursor.getColumnIndex(MediaStore.Images.ImageColumns.DATA);
+        return cursor.getString(idx);
     }
 }
